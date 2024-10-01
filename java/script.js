45eb74f2e48ddbccbd4ab1eed188f516f5ab7239
shais91 <muhammadshais1991@gmail.com>
2024-10-02 00:02:03 +0500

    grid size selection by form, css styling form code cleanup

--- java/script.js
+++ java/script.js
@@ -3,8 +3,33 @@ let firstDiv = document.createElement("div");
 firstDiv.setAttribute("class", "squareDiv");
 container.append(firstDiv);
 
-let gridNumber = 255;
+// Change Grid Size using html form element
+
+let changeGridSelection = document.getElementById("gridChangeForm");
+changeGridSelection.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let gridNumber;
+  const GRIDSIZE = document.getElementById("gridSize").value;
+  if (GRIDSIZE === "8x8") {
+    gridNumber = 128;
+    changeDivFunc(gridNumber);
+  } else if (GRIDSIZE === "32x32") {
+    gridNumber = 512;
+    changeDivFunc(gridNumber);
+  } else gridNumber = 256;
+  changeDivFunc(gridNumber);
+
+  console.log(`grid size : ${GRIDSIZE}, Grid internal number : ${gridNumber}`);
+  return;
+});
+
+// Function to change grid size
+
 let changeDivFunc = function (num) {
+  // empty the container
+  while (container.firstChild) {
+    container.firstChild.remove();
+  }
   for (let i = 0, grid; i < num; i++) {
     grid = document.createElement("div");
     grid.setAttribute("class", "squareDiv");
@@ -12,5 +37,5 @@ let changeDivFunc = function (num) {
   }
 };
 document.addEventListener("DOMContentLoaded", function () {
-  changeDivFunc(gridNumber);
+  changeDivFunc(128);
 });
